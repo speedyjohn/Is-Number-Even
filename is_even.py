ec5a5b22ee00d297e2a71a0d527847bdf49c24ee
speedyjohn <nachprog@gmail.com>
2025-11-05 09:02:02 +0500

    201-300

--- is_even.py
+++ is_even.py
@@ -402,6 +402,206 @@ def is_even(number: int) -> bool:
         return False
     elif number == 200:
         return True
+    elif number == 201:
+        return False
+    elif number == 202:
+        return True
+    elif number == 203:
+        return False
+    elif number == 204:
+        return True
+    elif number == 205:
+        return False
+    elif number == 206:
+        return True
+    elif number == 207:
+        return False
+    elif number == 208:
+        return True
+    elif number == 209:
+        return False
+    elif number == 210:
+        return True
+    elif number == 211:
+        return False
+    elif number == 212:
+        return True
+    elif number == 213:
+        return False
+    elif number == 214:
+        return True
+    elif number == 215:
+        return False
+    elif number == 216:
+        return True
+    elif number == 217:
+        return False
+    elif number == 218:
+        return True
+    elif number == 219:
+        return False
+    elif number == 220:
+        return True
+    elif number == 221:
+        return False
+    elif number == 222:
+        return True
+    elif number == 223:
+        return False
+    elif number == 224:
+        return True
+    elif number == 225:
+        return False
+    elif number == 226:
+        return True
+    elif number == 227:
+        return False
+    elif number == 228:
+        return True
+    elif number == 229:
+        return False
+    elif number == 230:
+        return True
+    elif number == 231:
+        return False
+    elif number == 232:
+        return True
+    elif number == 233:
+        return False
+    elif number == 234:
+        return True
+    elif number == 235:
+        return False
+    elif number == 236:
+        return True
+    elif number == 237:
+        return False
+    elif number == 238:
+        return True
+    elif number == 239:
+        return False
+    elif number == 240:
+        return True
+    elif number == 241:
+        return False
+    elif number == 242:
+        return True
+    elif number == 243:
+        return False
+    elif number == 244:
+        return True
+    elif number == 245:
+        return False
+    elif number == 246:
+        return True
+    elif number == 247:
+        return False
+    elif number == 248:
+        return True
+    elif number == 249:
+        return False
+    elif number == 250:
+        return True
+    elif number == 251:
+        return False
+    elif number == 252:
+        return True
+    elif number == 253:
+        return False
+    elif number == 254:
+        return True
+    elif number == 255:
+        return False
+    elif number == 256:
+        return True
+    elif number == 257:
+        return False
+    elif number == 258:
+        return True
+    elif number == 259:
+        return False
+    elif number == 260:
+        return True
+    elif number == 261:
+        return False
+    elif number == 262:
+        return True
+    elif number == 263:
+        return False
+    elif number == 264:
+        return True
+    elif number == 265:
+        return False
+    elif number == 266:
+        return True
+    elif number == 267:
+        return False
+    elif number == 268:
+        return True
+    elif number == 269:
+        return False
+    elif number == 270:
+        return True
+    elif number == 271:
+        return False
+    elif number == 272:
+        return True
+    elif number == 273:
+        return False
+    elif number == 274:
+        return True
+    elif number == 275:
+        return False
+    elif number == 276:
+        return True
+    elif number == 277:
+        return False
+    elif number == 278:
+        return True
+    elif number == 279:
+        return False
+    elif number == 280:
+        return True
+    elif number == 281:
+        return False
+    elif number == 282:
+        return True
+    elif number == 283:
+        return False
+    elif number == 284:
+        return True
+    elif number == 285:
+        return False
+    elif number == 286:
+        return True
+    elif number == 287:
+        return False
+    elif number == 288:
+        return True
+    elif number == 289:
+        return False
+    elif number == 290:
+        return True
+    elif number == 291:
+        return False
+    elif number == 292:
+        return True
+    elif number == 293:
+        return False
+    elif number == 294:
+        return True
+    elif number == 295:
+        return False
+    elif number == 296:
+        return True
+    elif number == 297:
+        return False
+    elif number == 298:
+        return True
+    elif number == 299:
+        return False
+    elif number == 300:
+        return True
     elif number == 34534521132230:
        return True
     else:
